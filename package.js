8af2d69c3339c2481743f845ccb7ff5ad2714e8a
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-04-01 14:13:53 +0530

    Bump version 1.3.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "Reactive Data Source for Search",
-  "version": "1.2.0",
+  "version": "1.3.0",
   "git": "https://github.com/meteorhacks/search-source.git",
   "name": "meteorhacks:search-source"
 });
